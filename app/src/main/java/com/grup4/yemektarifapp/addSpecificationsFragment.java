20a77210be7321925269d90cfcae1a20a6f2ca6a
Asrın Haktan <56225086+asrinhaktan@users.noreply.github.com>
2023-10-31 15:58:55 +0300

    add search_menu

--- app/src/main/java/com/grup4/yemektarifapp/addSpecificationsFragment.java
+++ app/src/main/java/com/grup4/yemektarifapp/addSpecificationsFragment.java
@@ -11,7 +11,6 @@ import android.view.ViewGroup;
 
 public class addSpecificationsFragment extends Fragment {
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
