00256c6b731c6d06b9da3f00b935b10e01bf935a
Ömer Faruk Topaloğlu <aaofm69@gmail.com>
2024-01-07 21:38:10 +0300

    Revert "left_drawer eklendi düzenlemeler yapıldı."
    This reverts commit 1578a8fee681795a5439b8100318173624ff5aa2.

--- app/src/main/java/com/grup4/yemektarifapp/Fragments/ProfileFragment.java
+++ app/src/main/java/com/grup4/yemektarifapp/Fragments/ProfileFragment.java
@@ -1,24 +1,23 @@
 package com.grup4.yemektarifapp.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
-
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.grup4.yemektarifapp.MainActivity;
 import com.grup4.yemektarifapp.R;
-
 import androidx.appcompat.widget.AppCompatButton;
 
 public class ProfileFragment extends Fragment {
 
     private AppCompatButton logoutButton;
-    private AppCompatButton googleAuthButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,35 +25,14 @@ public class ProfileFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_profile2, container, false);
 
         logoutButton = view.findViewById(R.id.btnLogoutButton);
-        googleAuthButton = view.findViewById(R.id.btnGoogleAuth);
-
-        // Sol drawer'ı tanımla
-        final DrawerLayout drawerLayout = view.findViewById(R.id.drawer_layout);
-
-        // Sol drawer içeriğini tanımla
-        final View leftDrawer = view.findViewById(R.id.left_drawer);
-
-        googleAuthButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Google Auth işlemleri burada gerçekleştirilebilir
-                // Sol drawer'ı aç
-                drawerLayout.openDrawer(leftDrawer);
-            }
-        });
-
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
-                // Sol drawer'ı kapat (çıkış yap butonu için)
-                drawerLayout.closeDrawer(leftDrawer);
-
                 // Çıkış işlemi sonrasında MainActivity'ye yönlendirme işlemi
                 replaceFragment(new HomeFragment());
             }
         });
-
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             String userName = user.getDisplayName();
@@ -71,13 +49,19 @@ public class ProfileFragment extends Fragment {
     }
 
     // Yeni eklenen metot: Fragment değiştirme işlemi
+    @Override
+    public void onStop() {
+        super.onStop();
+        replaceFragment(new HomeFragment());
+    }
+
     private void replaceFragment(Fragment fragment) {
         if (getActivity() != null) {
-            getActivity().getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.frame_layout, fragment)
-                    .addToBackStack(null)
-                    .commit();
+            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.frame_layout, fragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
         }
     }
 }
