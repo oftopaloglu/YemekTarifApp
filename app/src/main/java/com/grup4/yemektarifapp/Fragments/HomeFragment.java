da05ef38f6bfd37370057d7c85612da2a390f6b7
Ömer Faruk Topaloğlu <aaofm69@gmail.com>
2024-01-10 15:36:11 +0300

    anasayfa arama işlevi eklendi

--- app/src/main/java/com/grup4/yemektarifapp/Fragments/HomeFragment.java
+++ app/src/main/java/com/grup4/yemektarifapp/Fragments/HomeFragment.java
@@ -1,23 +1,40 @@
 package com.grup4.yemektarifapp.Fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
 
 import com.grup4.yemektarifapp.R;
-
+import com.grup4.yemektarifapp.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
+    private FragmentHomeBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+
+        binding.btnTarifAra.setOnClickListener(this::yemekArama);
+
+        return binding.getRoot();
+    }
+    private void yemekArama(View view) {
+        String editTarifAra = binding.editTarifAra.getText().toString();
+        Toast.makeText(getActivity(), editTarifAra, Toast.LENGTH_SHORT).show();
+        System.out.println(editTarifAra);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
