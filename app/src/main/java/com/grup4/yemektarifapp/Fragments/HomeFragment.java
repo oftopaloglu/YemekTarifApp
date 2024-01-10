6570036ecf8942a1b57dba3ea50225ba30b32ed5
Ömer Faruk Topaloğlu <aaofm69@gmail.com>
2024-01-10 15:49:58 +0300

    tarif arama fonksiyonu callback e bağlandı

--- app/src/main/java/com/grup4/yemektarifapp/Fragments/HomeFragment.java
+++ app/src/main/java/com/grup4/yemektarifapp/Fragments/HomeFragment.java
@@ -10,17 +10,26 @@ import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
+import com.grup4.yemektarifapp.Model.FoodRecipe;
 import com.grup4.yemektarifapp.R;
 import com.grup4.yemektarifapp.databinding.FragmentHomeBinding;
+import com.grup4.yemektarifapp.firebase.serach.FirestoreCallback;
+import com.grup4.yemektarifapp.firebase.serach.FirestoreFoodSearch;
+
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private FirestoreFoodSearch firestoreFoodSearch;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
+        firestoreFoodSearch = new FirestoreFoodSearch();
+
 
         binding.btnTarifAra.setOnClickListener(this::yemekArama);
 
@@ -28,8 +37,18 @@ public class HomeFragment extends Fragment {
     }
     private void yemekArama(View view) {
         String editTarifAra = binding.editTarifAra.getText().toString();
-        Toast.makeText(getActivity(), editTarifAra, Toast.LENGTH_SHORT).show();
-        System.out.println(editTarifAra);
+        firestoreFoodSearch.searchDocumentsByFieldName("tarifler", "name", editTarifAra, new FirestoreCallback() {
+            @Override
+            public void onCallback(List<FoodRecipe> list) {
+                // Handle the list of FoodRecipe objects here
+                Toast.makeText(getActivity(), "Found " + list.size() + " results", Toast.LENGTH_SHORT).show();
+
+                System.out.println("Found " + list.size() + " results");
+                for (FoodRecipe recipe : list) {
+                    System.out.println(recipe.toString());
+                }
+            }
+        });
     }
 
     @Override
