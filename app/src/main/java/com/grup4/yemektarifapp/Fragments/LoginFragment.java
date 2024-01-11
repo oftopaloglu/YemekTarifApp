539fa6d70721c60a84ceea6ccddda80f1000c986
Asrın Haktan <56225086+asrinhaktan@users.noreply.github.com>
2024-01-11 22:07:03 +0300

    login refactor
    login refactor add user  realtime database

--- app/src/main/java/com/grup4/yemektarifapp/Fragments/LoginFragment.java
+++ app/src/main/java/com/grup4/yemektarifapp/Fragments/LoginFragment.java
@@ -2,23 +2,23 @@ package com.grup4.yemektarifapp.Fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
@@ -28,19 +28,19 @@ import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.FirebaseDatabase;
 import com.grup4.yemektarifapp.R;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class LoginFragment extends Fragment {
 
-    Button googleAuth;
-    FirebaseAuth auth;
-    FirebaseDatabase database;
-    GoogleSignInClient mGoogleSignInClient;
-    int RC_SIGN_IN = 20;
+    private Button googleAuth;
+    private FirebaseAuth auth;
+    private FirebaseDatabase database;
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 20;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_login, container, false);
     }
 
@@ -48,6 +48,10 @@ public class LoginFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        initViews(view);
+    }
+
+    private void initViews(@NonNull View view) {
         googleAuth = view.findViewById(R.id.btnGoogleAuth);
         auth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
@@ -59,12 +63,11 @@ public class LoginFragment extends Fragment {
 
         mGoogleSignInClient = GoogleSignIn.getClient(getActivity(), gso);
 
-        googleAuth.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                googleSignIn();
-            }
-        });
+        googleAuth.setOnClickListener(this::onGoogleAuthClick);
+    }
+
+    private void onGoogleAuthClick(View view) {
+        googleSignIn();
     }
 
     private void googleSignIn() {
@@ -76,15 +79,17 @@ public class LoginFragment extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == RC_SIGN_IN){
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+        if (requestCode == RC_SIGN_IN) {
+            handleGoogleSignInResult(data);
+        }
+    }
 
-            try {
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                firebaseAuth(account.getIdToken());
-            } catch (Exception e) {
-                Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
-            }
+    private void handleGoogleSignInResult(Intent data) {
+        try {
+            GoogleSignInAccount account = GoogleSignIn.getSignedInAccountFromIntent(data).getResult(ApiException.class);
+            firebaseAuth(account.getIdToken());
+        } catch (Exception e) {
+            Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -92,29 +97,39 @@ public class LoginFragment extends Fragment {
         AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
 
         auth.signInWithCredential(credential)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            FirebaseUser user = auth.getCurrentUser();
-
-                            // Yeni eklenen kısım: Kullanıcı giriş yaptıktan sonra HomeFragment'a yönlendir
-                            replaceFragment(new HomeFragment());
-
-                            HashMap<String, Object> map = new HashMap<>();
-                            map.put("id", user.getUid());
-                            map.put("name", user.getDisplayName());
-                            map.put("profile", user.getPhotoUrl().toString());
-
-                            database.getReference().child("users").child(user.getUid()).setValue(map);
-                        } else {
-                            Toast.makeText(getActivity(), "Authentication failed.", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+                .addOnCompleteListener(this::onAuthComplete);
     }
 
-    // Yeni eklenen metot: Fragment değiştirme işlemi
+    private void onAuthComplete(@NonNull Task<AuthResult> task) {
+        if (task.isSuccessful()) {
+            FirebaseUser user = auth.getCurrentUser();
+            replaceFragment(new HomeFragment());
+            saveUserToDatabase(user);
+        } else {
+            Toast.makeText(getActivity(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void saveUserToDatabase(FirebaseUser user) {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("id", user.getUid());
+        map.put("name", user.getDisplayName());
+        map.put("profile", user.getPhotoUrl().toString());
+
+        // FavRecipes örneği
+        ArrayList<String> favRecipes = new ArrayList<>();
+
+        map.put("favRecipes", favRecipes);
+
+        // MyRecipes örneği
+        ArrayList<String> myRecipes = new ArrayList<>();
+        map.put("MyRecipes", myRecipes);
+
+        database.getReference().child("users").child(user.getUid()).setValue(map);
+    }
+
+
+
     private void replaceFragment(Fragment fragment) {
         if (getActivity() != null) {
             FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
