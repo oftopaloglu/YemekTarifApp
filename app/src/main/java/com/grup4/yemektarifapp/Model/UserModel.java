71255c07040f46b347ffa4d969a54ca455f38b73
Asrın Haktan <56225086+asrinhaktan@users.noreply.github.com>
2024-01-11 21:42:33 +0300

    :bug: bug fix  model

--- app/src/main/java/com/grup4/yemektarifapp/Model/UserModel.java
+++ app/src/main/java/com/grup4/yemektarifapp/Model/UserModel.java
@@ -1,46 +1,43 @@
 package com.grup4.yemektarifapp.Model;
 
+import java.util.List;
+
 public class UserModel {
-    String id;
-    String name;
-    String favRecipe;
-    String myRecipe;
-    public UserModel(String id, String name, String favRecipe, String myRecipe) {
+    private String id;
+    private String name;
+    private List<String> favRecipe;
+    private List<String> MyRecipe;
+
+    public UserModel(String id, String name, List<String> favRecipe, List<String> myRecipe) {
         this.id = id;
         this.name = name;
         this.favRecipe = favRecipe;
-        this.myRecipe = myRecipe;
+        MyRecipe = myRecipe;
     }
     public String getId() {
         return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getFavRecipe() {
+    public List<String> getFavRecipe() {
         return favRecipe;
     }
 
-    public void setFavRecipe(String favRecipe) {
-        this.favRecipe = favRecipe;
+    public List<String> getMyRecipe() {
+        return MyRecipe;
     }
 
-    public String getMyRecipe() {
-        return myRecipe;
-    }
-
-    public void setMyRecipe(String myRecipe) {
-        this.myRecipe = myRecipe;
+    @Override
+    public String toString() {
+        return "UserModel{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", favRecipe=" + favRecipe +
+                ", MyRecipe=" + MyRecipe +
+                '}';
     }
 
 
